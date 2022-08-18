6fc3aab21b7ed6b71affa5bdd3d497457846b651
Salvatore Manna <salvm93@gmail.com>
2022-08-18 11:21:53 +0200

    Firebase integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgszmEgSrFvWvlr69CSk4kXMO6lREzSjc",
+  authDomain: "commit-history-test.firebaseapp.com",
+  projectId: "commit-history-test",
+  storageBucket: "commit-history-test.appspot.com",
+  messagingSenderId: "878217647024",
+  appId: "1:878217647024:web:dc14841fb2991794747c75"
+};
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
